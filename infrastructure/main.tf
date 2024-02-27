f858987f73ccb12367e087572f61f7cc545697bd
Mahi Fernando <mahi.fernando1@nhs.net>
2024-02-27 11:27:32 +0000

    chore: resource group added for checking tf tools chain

---
+++ infrastructure/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "landing-zone-demo-rg"
+  location = "uksouth"
+}
